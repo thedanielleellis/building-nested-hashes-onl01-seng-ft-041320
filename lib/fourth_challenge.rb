911b7e4db2f3497e1927d1d39b2dae3d7ece4333
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-21 04:12:02 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,12 +1,13 @@
 def fourth_challenge
+
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [ {name: "Benvolio", age: "17", attitude: "worried"},
-                          {name: "Mercutio", age: "18", attitude: "hot-headed"}
-
+      :hero_friends => [
+         {name: "Mercutio", age: "18", attitude: "hot-headed"},
+         {name: "Benvolio", age: "17", attitude: "worried"}
       ]
    },
    :capulet => {
@@ -14,11 +15,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-        {name: "Nurse", age: "44", attitude: "worried"}
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-}
+  }
 
 
 
